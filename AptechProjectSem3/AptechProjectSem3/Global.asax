d4b3befb16a5ba244f7a3b21a3e8b0c569a2e5bc
Tung Nguyen <tung.hdv@gmail.com>
2018-10-23 19:44:01 +0700

    commit all

---
+++ AptechProjectSem3/AptechProjectSem3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AptechProjectSem3.MvcApplication" Language="C#" %>
---
+++ AptechProjectSem3/AptechProjectSem3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AptechProjectSem3.Startup))]
+namespace AptechProjectSem3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
