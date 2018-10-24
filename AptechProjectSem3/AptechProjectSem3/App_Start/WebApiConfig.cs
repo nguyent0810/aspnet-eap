45ab896c4c6a3c587c49c1f183ff21a84f399129
tung.nguyen <tung.nguyen@sioux.asia>
2018-10-24 09:46:31 +0700

    Examination 1

--- AptechProjectSem3/AptechProjectSem3/App_Start/WebApiConfig.cs
+++ AptechProjectSem3/AptechProjectSem3/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace AptechProjectSem3
 {
@@ -19,6 +20,16 @@ namespace AptechProjectSem3
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //By default Web API return XML data
+            //We can remove this by clearing the SupportedMediaTypes options as follows
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+
+            //Now set the serializer for JsonFormatter to Indented to get Json Formatted data
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+            //For converting data in Camel Case
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
---
+++ AptechProjectSem3/AptechProjectSem3/dbo.WorkItems.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[WorkItems] ON
+INSERT INTO [dbo].[WorkItems] ([Id], [Title], [Status], [CreatedDate], [CompletedDate]) VALUES (4, N'Food', N'On Progress', N'2018-02-10', N'2018-03-10')
+INSERT INTO [dbo].[WorkItems] ([Id], [Title], [Status], [CreatedDate], [CompletedDate]) VALUES (5, N'Coffee', N'Done', N'2018-03-23', N'2018-04-25')
+INSERT INTO [dbo].[WorkItems] ([Id], [Title], [Status], [CreatedDate], [CompletedDate]) VALUES (6, N'Toalet Paper', N'New', N'2018-10-29', N'2018-11-29')
+SET IDENTITY_INSERT [dbo].[WorkItems] OFF
